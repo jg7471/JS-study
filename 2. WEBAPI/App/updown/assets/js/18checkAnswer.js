d4f665190cc61856b249a182e05b25530474ced0
JG <iamjgl@naver.com>
2024-02-19 09:03:11 +0900

    d123

---
+++ 2. WEBAPI/App/updown/assets/js/18checkAnswer.js
@@ -0,0 +1,39 @@
+function caseUp($icon){
+    document.getElementById('begin').textContent = +$icon.dataset.iconNumber + 1;
+    
+    document.getElementById('down').classList.remove('selected');
+    document.getElementById('up').classList.add('selected');
+
+    const $numbers = document.getElementById('numbers');
+
+    let $delTarget = $icon;
+
+    while($delTarget){
+
+        const $nextTarget = $delTarget.previousElmentSibling;
+        $numbers.removeChild($delTarget);
+        $delTarget = $nextTarget;
+    }
+
+
+}
+
+
+function caseUp($icon){
+    document.getElementById('begin').textContent.dataset.iconNumber +1;
+    
+    document.getElementById('up').classList.add('selected');
+    document.getElementById('down').classList.remove('selected');
+
+    const $numbers = document.getElementById('numbers');
+
+    let $delTarget = $icon
+    while($delTarget){
+        $nextTarget = $delTarget.previousElmentSibling;
+        $numbers.removeChild($delTarget);
+        $delTarget = $nextTarget;
+
+    }
+
+
+}
